255686667221d5249bbb95338cb251312c94c4b0
AbiRodStr <abistrain23@gmail.com>
2025-03-02 17:47:54 +0100

    form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = "This is a poem";
 
+  new Typewriter("#poem", {
+    strings: "This is a poem",
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
